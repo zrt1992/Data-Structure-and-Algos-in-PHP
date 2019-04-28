5562335b59f718a1e185d1f4327ab12b367f27b5
kukfi@hotmail.com <zulfiqar_tariq@hotmail.com>
2019-04-28 20:03:56 +0500

    ad

---
+++ packagetest/vendor/zulfi/zulfi/src/MyPackage/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\MyPackage;
+
+class Test
+{
+    public static function abc (){
+        echo 'my test';
+    }
+
+}
---
+++ packagetest/vendor/zulfi/zulfi/test/test.php
@@ -0,0 +1,5 @@
+<?php
+require '../vendor/autoload.php';
+
+use App\MyPackage\Test;
+Test::abc();
